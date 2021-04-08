97eb9bd9ee23b09a7b370ee3054460d53e5172fe
Abdulla-Mohamed <s18377@pjwstk.edu.pl>
2021-04-08 12:53:43 +0200

    Create class Model Game

---
+++ Game-List-Solution/Game-List/Models/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Game_List.Models
+{
+    [Serializable]
+    public class Game
+    {
+        public int IDGame { get; set; }
+        [Required(ErrorMessage = "Game name required")]
+        public string GameName { get; set; }
+
+        public string GameDescription { get; set; }
+        [Required(ErrorMessage = "Category name required")]
+        public string GameCategory { get; set; }
+        [Required(ErrorMessage = "Device name required")]
+        public string Device  { get; set; }
+    }
+}
