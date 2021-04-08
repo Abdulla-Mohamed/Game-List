a7862074127c21a4c15717bc005679012b5489fd
Abdulla-Mohamed <s18377@pjwstk.edu.pl>
2021-04-08 12:54:19 +0200

    Create class model User

---
+++ Game-List-Solution/Game-List/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Game_List.Models
+{
+    [Serializable]
+    public class User
+    {
+        [Required(ErrorMessage = "Login required")]
+        [EmailAddress]
+        public string login { get; set; }
+
+        [Required(ErrorMessage = "Password required")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Username required")]
+        public string Username { get; set; }
+        public string Bio { get; set; }
+    }
+}
